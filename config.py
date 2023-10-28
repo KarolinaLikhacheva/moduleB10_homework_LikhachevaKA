c753f4e6ac1a6a70af7f43c43f7dce8bd0bbd956
Karolina <123109500+KarolinaLikhacheva@users.noreply.github.com>
2023-10-28 11:25:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-API_KEY = '2BccUD8B7vBgLRJwCjYSA5oUeAzxKg9Q'
 
 TOKEN = '6502330334:AAF0o-M4XBpQ1-r9KFwzzMyy_CSXCnqYxa8'    #  Мой ключ-доступ к боту
 currencies = {    #  Список конвертируемых валют в виде списка ключ-значений
